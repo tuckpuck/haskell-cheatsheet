ecb16228cc4ebc8006db7ba25edde92c230673ce
Tucker Triggs <tuckertriggs@pm.me>
2023-05-11 17:23:43 +0800

    improve types section

--- 1_types.hs
+++ 1_types.hs
@@ -1,10 +1,9 @@
 -- * Type Info
--- Types are like a label that is attached to data that defines and limits what that data can do. To attach them, use :: operator
--- The line with the :: operator is called a type signature, it is read as "has type of". 
--- The type of any expression, value, or function can be explicitly stated. It will be inferred by Haskell's compiler if not explicitly stated. 
--- There is no untyped data in Haskell.
--- Every expression's type is known at compile time. If there are types that don't work, the program won't compile
--- Type checking is the process of verifying and enforcing constraints on types. This helps avoid preventable mistakes. Code behaves more predictably due to the focus on types.
+-- Types are like a label that is attached to data that defines and limits what that data can do.
+-- To attach them, use :: operator, where you "set the type". The entire line with the :: operator is called the type signature, it is read as "has type of".
+-- The type of any expression, value, or function can be explicitly stated, otherwise it will be inferred by Haskell's compiler.
+-- Every expression's type is known at compile time. If there are types that don't work, the program won't compile.
+-- Type checking is the process of verifying and enforcing constraints on data. This helps avoid preventable mistakes. Code behaves more predictably due to the focus on types.
 
 -- * Checking Type
 -- :t command is used to tell what type something is
@@ -15,8 +14,8 @@
 -- :t 4 == 5 returns Bool
 
 -- * Type signature
--- The type signature (first line) sets the function name and the types of params
--- The function definition (second line) sets what is executed when the function is called
+-- The type signature (first line) sets the function name and types of the parameters and what it it expected to return. 
+-- The function definition (second line) is  what is executed when the function is called
 -- Params are separated by ->, with return type always coming last
 sumAndAddTwo :: Int -> Int -> Int
 sumAndAddTwo x y = x + y + 2
@@ -27,8 +26,10 @@ addThree x y z = x + y + z
 
 -- * Type inference
 -- If not given a type, Haskell will infer it based on requirements of functions 
-london u = "London " ++ u
-demoTypeInference = london "hey" --"London hey"
+expectsString u = "hey " ++ u
+mustPassString = expectsString "London" --"hey London"
+expectsList u = ["Tokyo"] ++ u
+mustPassList = expectsList ["New York"] --["Tokyo","New York"]
 
 -- * Native Types Overview
 -- Integral numbers
@@ -53,18 +54,19 @@ intType = 3 :: Int --Int
 -- This is about the maximum size you can do for Int type numbers
 maxInt :: Int
 maxInt = 2^62
--- This will result in 0 on 64 bit CPUs because it is over the size limit 
+-- This should result in 0 on 64 bit CPUs because it is over the size limit 
 overMaxInt :: Int
 overMaxInt = 2^64
 
 ------ * Integer
 -- Integer is arbitrary precision type: it can hold any integer no matter how big
-integerType = 382938834249328432943982483289 --Integer 
+integerType = 38293883424932843294398248320972389473927432974392749328289 --Integer 
 -- Numbers default to integer if not specifically defined 
 
 -- * Fractional Types
 -- Decimal numbers default to double if not specifically defined
 -- Float allows for faster performance, but it is still recommended to use double in general so you don't encounter rounding errors. 
+
 
 ------ * Float
 -- Float is a real floating point number with single precision (32 bits)
@@ -77,7 +79,7 @@ floatType = 2.0 :: Float --Float
 doubleType = 3.5543543543 --Double
 
 ------ * Rational
---When even more precision is needed, use the Rational data type because it doesn't have the same rounding as floating-point arithmetic. It is a specialization of the Ratio type. 
+-- When even more precision is needed, use the Rational data type because it doesn't have the same rounding features as floating-point.  
 rationalType = 3.324325364 :: Rational --Rational 
 
 -- * Bool 
@@ -99,7 +101,7 @@ charType = 'a' --Char
 -- Strings are lists of characters. 
 -- Strings are surrounded by double quotes. Can also be expressed as a list of Chars. 
 -- String type is the same as [Char] type. 
--- Because strings are really [Char] lists, these all are essentially equal and have the same type:
+-- Because strings are really [Char] lists, these all are essentially equivalent and have the same type:
 stringType = "Tucker" --[Char]
 stringType2 = ['T','u','c','k','e','r'] --[Char]
 stringType3 = 'T':'u':'c':'k':'e':'r':[] --[Char]
@@ -107,5 +109,12 @@ stringType3 = 'T':'u':'c':'k':'e':'r':[] --[Char]
 -- * Type variables aka polymorphism
 -- Polymorphic types - A polymorphic value can have multiple types. This is useful in cases where you want to accept multiple types of data, or if the data type is not important (ex. a function that returns the first of two values). 
 -- While specific types are capitalized (Bool), polymorphic types are lower case (a, b, c, etc.). 'a' can represent any type, but all variables with the type 'a' must be the same type. Checking the types for fst and snd is a good illustration of this. 
--- ex. length :: [a] -> Int... a is used because because the type function works on any type of list (numbers, letters, etc.)
--- ex. fst :: (a, b) -> a...  a and b are used because they can be different types
+-- The length function uses the 'a' type because the length function can work on multiple types (numbers, lists, etc.).
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length xs
+
+-- Fst is another example where 'a' and 'b' are are polymorphic types because the types within the tuple can be any type.
+fst' :: (a, b) -> b
+fst' (_, y) = y
+\
